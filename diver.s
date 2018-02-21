7ac57491d835986c7a7877d7def9c715dc070adb
jasminko <betelge@gmail.com>
2018-02-21 17:51:48 +0700

    Add a moving sprite OAM DMA upload Dummy char data

--- diver.s
+++ diver.s
@@ -8,12 +8,18 @@
 	.byte $01
 	.byte $00
 
+; ppu hardware
 PPUCTRL = $2000
 PPUMASK = $2001
 PPUSTATUS = $2002
+OAMADDR = $2003
 PPUSCROLL = $2005
 PPUADDR = $2006
 PPUDATA = $2007
+OAM_DMA = $4014
+
+; local defines
+OAM = $0200
 
 .segment "STARTUP"
 
@@ -51,6 +57,28 @@ paletteloop:
 	cpx #32
 	bne paletteloop
 
+; Set oam
+	ldx #0
+	lda #$80 ; y pos
+	sta OAM, x
+	inx
+	lda #$00 ; tile index
+	sta OAM, x
+	inx
+	lda #%00000001 ; vhP___pp
+	sta OAM, x
+	inx
+	lda #$80 ; x pos
+	sta OAM, x
+	inx
+
+; Start ppu
+	lda #%10001000
+	sta PPUCTRL
+	lda #%00011110
+	sta PPUMASK
+	cli
+
 loop:
 	jmp loop
 
@@ -59,7 +87,16 @@ palette:
 	.byte $10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$1A,$1B,$1C,$1D,$1E,$1F
 
 NMI:
+	inc OAM
+; Upload oam
+	lda #0
+	sta OAMADDR ; Point oam address to start of oam memory
+	lda #>OAM
+	sta OAM_DMA ; Point the dma to top byte of the oam address in ram
+	rti
+
 IRQ:
+	rti
 
 .segment "VECTORS"
 	.word NMI
@@ -67,3 +104,11 @@ IRQ:
 	.word IRQ
 
 .segment "CHARS"
+; tiles
+	.repeat $100
+	.byte $55,$56,$57,$58,$59,$5A,$65,$75,$85,$95,$A5,$55,$55,$55,$55,$55
+	.endrepeat
+; sprites
+	.repeat $1000
+	.byte $ff
+	.endrepeat
